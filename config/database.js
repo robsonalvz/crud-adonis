47c9b9782dc272756522ce20908d0b30a8dfb6ea
robsonalvz <robson.alves@dce.ufpb.br>
2019-07-21 23:56:12 -0300

    first commit

--- config/database.js
+++ config/database.js
@@ -2,8 +2,10 @@
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
+
 const Url = require('url-parse')
 const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
